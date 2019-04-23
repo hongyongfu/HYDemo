5bcbf4ee6bcca9037f591798057065026b444b88
fuhongyong <446872912@qq.com>
2019-04-23 16:24:02 +0800

    整理

---
+++ HYProject/Classes/AViewController.h
@@ -0,0 +1,13 @@
+//
+//  AViewController.h
+//  HYProject
+//
+//  Created by hongyong fu on 2019/4/23.
+//  Copyright © 2019年 SIBU Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AViewController : UIViewController
+
+@end
---
+++ HYProject/Classes/AViewController.m
@@ -0,0 +1,24 @@
+//
+//  AViewController.m
+//  HYProject
+//
+//  Created by hongyong fu on 2019/4/23.
+//  Copyright © 2019年 SIBU Group. All rights reserved.
+//
+
+#import "AViewController.h"
+
+@interface AViewController ()
+
+@end
+
+@implementation AViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor greenColor];
+}
+
+
+
+@end
